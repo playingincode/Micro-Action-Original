6c4f18af3710d4bbabb196d43fd84754259be570
npoddar <npoddar@nef-devel2.inria.fr>
2025-07-18 15:59:00 +0200

    frozen all our models

--- mar_scripts/manet/mmaction2/configs/_base_/models/manet_r50.py
+++ mar_scripts/manet/mmaction2/configs/_base_/models/manet_r50.py
@@ -1,179 +1,3 @@
-# # model settings
-# model = dict(
-#     type='Recognizer2D_ours',
-#     backbone=dict(
-#         type='ResNetTSM',
-#         pretrained='torchvision://resnet50',
-#         depth=50,
-#         norm_eval=False,
-#         shift_div=8),
-#     cls_head=dict(
-#         type='MANetHead_ours',
-#         num_classes=6,
-#         in_channels=1408,
-#         spatial_type='avg',
-#         consensus=dict(type='AvgConsensus', dim=1),
-#         dropout_ratio=0.5,
-#         init_std=0.001,
-#         is_shift=True),
-#     # model training and testing settings
-#     train_cfg=None,
-#     test_cfg=dict(average_clips='prob'))
-
-
-# body_head_model = dict(
-#     type='Recognizer2D',
-#     backbone=dict(
-#         type='ResNetTSM',
-#         pretrained='torchvision://resnet34',
-#         depth=34,
-#         norm_eval=False,
-#         shift_div=8),
-#     cls_head=dict(
-#         type='MANetHead',
-#         num_classes=11,
-#         in_channels=512,
-#         spatial_type='avg',
-#         consensus=dict(type='AvgConsensus', dim=1),
-#         dropout_ratio=0.5,
-#         init_std=0.001,
-#         is_shift=True),
-#     # model training and testing settings
-#     train_cfg=None,
-#     test_cfg=dict(average_clips='prob'))
-
-
-# upper_limb_model = dict(
-#     type='Recognizer2D',
-#     backbone=dict(
-#         type='ResNetTSM',
-#         pretrained='torchvision://resnet34',
-#         depth=34,
-#         norm_eval=False,
-#         shift_div=8),
-#     cls_head=dict(
-#         type='MANetHead',
-#         num_classes=13,
-#         in_channels=512,
-#         spatial_type='avg',
-#         consensus=dict(type='AvgConsensus', dim=1),
-#         dropout_ratio=0.5,
-#         init_std=0.001,
-#         is_shift=True),
-#     # model training and testing settings
-#     train_cfg=None,
-#     test_cfg=dict(average_clips='prob'))
-
-
-# lower_limb_model = dict(
-#     type='Recognizer2D',
-#     backbone=dict(
-#         type='ResNetTSM',
-#         pretrained='torchvision://resnet34',
-#         depth=34,
-#         norm_eval=False,
-#         shift_div=8),
-#     cls_head=dict(
-#         type='MANetHead',
-#         num_classes=8,
-#         in_channels=512,
-#         spatial_type='avg',
-#         consensus=dict(type='AvgConsensus', dim=1),
-#         dropout_ratio=0.5,
-#         init_std=0.001,
-#         is_shift=True),
-#     # model training and testing settings
-#     train_cfg=None,
-#     test_cfg=dict(average_clips='prob'))
-
-
-# body_hand_model = dict(
-#     type='Recognizer2D_ours',
-#     backbone=dict(
-#         type='ResNetTSM',
-#         pretrained='torchvision://resnet50',
-#         depth=50,
-#         norm_eval=False,
-#         shift_div=8),
-#     cls_head=dict(
-#         type='MANetHead_ours',
-#         num_classes=6,
-#         in_channels=1408,
-#         spatial_type='avg',
-#         consensus=dict(type='AvgConsensus', dim=1),
-#         dropout_ratio=0.5,
-#         init_std=0.001,
-#         is_shift=True),
-#     # model training and testing settings
-#     train_cfg=None,
-#     test_cfg=dict(average_clips='prob'))
-
-
-# head_hand_model = dict(
-#     type='Recognizer2D_ours',
-#     backbone=dict(
-#         type='ResNetTSM',
-#         pretrained='torchvision://resnet50',
-#         depth=50,
-#         norm_eval=False,
-#         shift_div=8),
-#     cls_head=dict(
-#         type='MANetHead_ours',
-#         num_classes=10,
-#         in_channels=1408,
-#         spatial_type='avg',
-#         consensus=dict(type='AvgConsensus', dim=1),
-#         dropout_ratio=0.5,
-#         init_std=0.001,
-#         is_shift=True),
-#     # model training and testing settings
-#     train_cfg=None,
-#     test_cfg=dict(average_clips='prob'))
-
-# leg_hand_model = dict(
-#     type='Recognizer2D_ours',
-#     backbone=dict(
-#         type='ResNetTSM',
-#         pretrained='torchvision://resnet50',
-#         depth=50,
-#         norm_eval=False,
-#         shift_div=8),
-#     cls_head=dict(
-#         type='MANetHead_ours',
-#         num_classes=4,
-#         in_channels=1408,
-#         spatial_type='avg',
-#         consensus=dict(type='AvgConsensus', dim=1),
-#         dropout_ratio=0.5,
-#         init_std=0.001,
-#         is_shift=True),
-#     # model training and testing settings
-#     train_cfg=None,
-#     test_cfg=dict(average_clips='prob'))
-
-
-# combined_model = dict(
-#     type='MultiBranchModel',
-#     backbone=dict(
-#         type='ResNetTSM',
-#         pretrained='torchvision://resnet50',
-#         depth=50,
-#         norm_eval=False,
-#         shift_div=8),
-#     cls_head=dict(
-#         type='MANetHead_ours',
-#         num_classes=4,
-#         in_channels=1408,
-#         spatial_type='avg',
-#         consensus=dict(type='AvgConsensus', dim=1),
-#         dropout_ratio=0.5,
-#         init_std=0.001,
-#         is_shift=True),
-#     # model training and testing settings
-#     train_cfg=None,
-#     test_cfg=dict(average_clips='prob'))
-
-
 model = dict(
     type='MultiBranchModel',
     main_model=dict(
@@ -198,18 +22,18 @@ model = dict(
         test_cfg=dict(average_clips='prob')
     ),
     body_head_model=dict(
-        type='Recognizer2D',
-        pretrained='/data/stars/user/areka/MULTIMEDIA_CONFERANCE_2025/MICRO_6CLASSES/Micro-Action-Original/mar_scripts/manet/mmaction2/work_dirs/manet_resnet34_body_head/best_top1_acc_epoch_39.pth',
+        type='Recognizer2D_ours',
+        pretrained='/data/stars/user/npoddar/Micro-Action/mar_scripts/manet/mmaction2/work_dirs/manet_prova_sgp_head_body_passive/best_top1_acc_epoch_31.pth',
         backbone=dict(
             type='ResNetTSM',
-            pretrained='torchvision://resnet34',
-            depth=34,
+            pretrained='torchvision://resnet50',
+            depth=50,
             norm_eval=False,
             shift_div=8),
         cls_head=dict(
-            type='MANetHead',
+            type='MANetHead_ours',
             num_classes=11,
-            in_channels=512,
+            in_channels=1408,
             spatial_type='avg',
             consensus=dict(type='AvgConsensus', dim=1),
             dropout_ratio=0.5,
@@ -219,18 +43,18 @@ model = dict(
         test_cfg=dict(average_clips='prob')
     ),
     upper_limb_model=dict(
-        type='Recognizer2D',
-        pretrained='/data/stars/user/areka/MULTIMEDIA_CONFERANCE_2025/MICRO_6CLASSES/Micro-Action-Original/mar_scripts/manet/mmaction2/work_dirs/manet_resnet34_upper_limb/best_top1_acc_epoch_58.pth',
+        type='Recognizer2D_ours',
+        pretrained='/data/stars/user/npoddar/Micro-Action/mar_scripts/manet/mmaction2/work_dirs/manet_prova_sgp_upper_limb_passive/best_top1_acc_epoch_39.pth',
         backbone=dict(
             type='ResNetTSM',
-            pretrained='torchvision://resnet34',
-            depth=34,
+            pretrained='torchvision://resnet50',
+            depth=50,
             norm_eval=False,
             shift_div=8),
         cls_head=dict(
-            type='MANetHead',
+            type='MANetHead_ours',
             num_classes=13,
-            in_channels=512,
+            in_channels=1408,
             spatial_type='avg',
             consensus=dict(type='AvgConsensus', dim=1),
             dropout_ratio=0.5,
@@ -240,18 +64,18 @@ model = dict(
         test_cfg=dict(average_clips='prob')
     ),
     lower_limb_model=dict(
-        type='Recognizer2D',
-        pretrained='/data/stars/user/areka/MULTIMEDIA_CONFERANCE_2025/MICRO_6CLASSES/Micro-Action-Original/mar_scripts/manet/mmaction2/work_dirs/manet_resnet34_lower_limb/best_top1_acc_epoch_64.pth',
+        type='Recognizer2D_ours',
+        pretrained='/data/stars/user/npoddar/Micro-Action/mar_scripts/manet/mmaction2/work_dirs/manet_prova_sgp_lower_limb_passive/best_top1_acc_epoch_57.pth',
         backbone=dict(
             type='ResNetTSM',
-            pretrained='torchvision://resnet34',
-            depth=34,
+            pretrained='torchvision://resnet50',
+            depth=50,
             norm_eval=False,
             shift_div=8),
         cls_head=dict(
-            type='MANetHead',
+            type='MANetHead_ours',
             num_classes=8,
-            in_channels=512,
+            in_channels=1408,
             spatial_type='avg',
             consensus=dict(type='AvgConsensus', dim=1),
             dropout_ratio=0.5,
