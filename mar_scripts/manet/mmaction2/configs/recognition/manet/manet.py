dd9eb29348d8406c93e8006102e1b9a8b4bea3e8
npoddar <npoddar@nef-devel2.inria.fr>
2025-07-18 08:28:07 +0200

    manet_final_with_manet_and_some_our_experts

--- mar_scripts/manet/mmaction2/configs/recognition/manet/manet.py
+++ mar_scripts/manet/mmaction2/configs/recognition/manet/manet.py
@@ -100,4 +100,4 @@ optimizer = dict(
 )
 # runtime settings
 checkpoint_config = dict(interval=5)
-work_dir = './work_dirs/all_our_experts_and_gate_updated_with_manet'
+work_dir = './work_dirs/all_experts_with_some_manet_and_some_ours_gate_updated'
--- mar_scripts/manet/mmaction2/train_runner.sh
+++ mar_scripts/manet/mmaction2/train_runner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #OAR -p gpu='YES' and host='nefgpu46.inria.fr'
 #OAR -l /nodes=1/gpunum=1,walltime=72:00:00
-#OAR --name all_our_experts_and_gate_updated
+#OAR --name all_experts_with_some_manet_and_some_ours_gate_and_manet_added_at_end
 #OAR --stdout nef_logs/%jobname%.%jobid%.out
 #OAR --stderr nef_logs/%jobname%.%jobid%.err
 
