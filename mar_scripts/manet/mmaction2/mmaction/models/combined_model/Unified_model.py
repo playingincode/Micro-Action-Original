834ec7d96dd7b6aab5d8758afebed94db10f0c7e
npoddar <npoddar@nef-devel2.inria.fr>
2025-07-25 13:40:19 +0200

    without_cross_attention

--- mar_scripts/manet/mmaction2/mmaction/models/combined_model/Unified_model.py
+++ mar_scripts/manet/mmaction2/mmaction/models/combined_model/Unified_model.py
@@ -108,7 +108,8 @@ class CrossAttentionWithTransformer(nn.Module):
         """
         # Step 1: Cross-attention per time step
         # print("Gate weights",gate_weights.shape,"Expert outputs",expert_outputs.shape)
-        x = self.cross_attn(gate_weights, expert_outputs)  # [B*T, 1408]
+        # x = self.cross_attn(gate_weights, expert_outputs)  # [B*T, 1408]
+        x = expert_outputs.mean(dim=1)  # [B*T, 1408]
         # print("After cross attention",x.shape)
         # fused = torch.cat([x.mean(dim=1), out_manet_model], dim=1)
         # fused=out_manet_model+
