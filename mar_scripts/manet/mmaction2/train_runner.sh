4cebdfb60bfb825b8733b72aa0adacea922d18b6
npoddar <npoddar@nef-devel2.inria.fr>
2025-07-26 10:44:54 +0200

    i3d put to run again

--- mar_scripts/manet/mmaction2/train_runner.sh
+++ mar_scripts/manet/mmaction2/train_runner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #OAR -p gpu='YES' and host='nefgpu56.inria.fr'
 #OAR -l /nodes=1/gpunum=1,walltime=72:00:00
-#OAR --name I3d_MPIIGroupInteraction_single_interval_val
+#OAR --name I3d_MPIIGroupInteraction_evaluation_changed_to_single_intervals_again
 #OAR --stdout nef_logs/%jobname%.%jobid%.out
 #OAR --stderr nef_logs/%jobname%.%jobid%.err
 
