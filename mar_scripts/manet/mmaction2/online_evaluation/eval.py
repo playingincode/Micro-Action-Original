5b60980dc49b7ac60cadfb5f047bda30fe8a839e
npoddar <npoddar@nefgpu41.inria.fr>
2025-07-30 14:52:18 +0200

    test modified for confusion matrix

--- mar_scripts/manet/mmaction2/online_evaluation/eval.py
+++ mar_scripts/manet/mmaction2/online_evaluation/eval.py
@@ -8,10 +8,11 @@ import pickle
 import zipfile
 import csv
 import numpy as np
-
-pickle_file_path = 'online_evaluation/test_result_pcan.pickle'
-pred_file_path = 'online_evaluation/prediction.csv'
-zip_file_path = 'online_evaluation/submission.zip'
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+pickle_file_path = 'online_evaluation/test_result_our_model_on_mpii.pickle'
+pred_file_path = 'online_evaluation/predicition_our_model_mpii.csv'
+zip_file_path = 'online_evaluation/submission_our_model_mpii.zip'
 
 def fine2coarse(x):
     if x <= 4:
@@ -31,9 +32,43 @@ def fine2coarse(x):
 
 with open(pickle_file_path, 'rb') as file:
     datas = pickle.load(file)
+    # print(datas)
 
-with open('./data/ma52/test_list_videos.txt', 'r') as f:
-    file_names = [line.strip().split()[0] for line in f.readlines()]
+with open('/data/stars/user/npoddar/val_list_videos_mpII.txt', 'r') as f:
+    lines = f.readlines()
+    file_names = [line.strip().split()[0] for line in lines]
+    labels = [int(line.strip().split()[1]) for line in lines]
+    y_true = []
+    y_pred = []
+    for pred_scores, label in zip(datas, labels):
+
+        pred_label = int(np.argmax(pred_scores))
+
+        y_true.append(label)
+        y_pred.append(pred_label)
+
+        # Top-1 accuracy
+    correct = sum(1 for t, p in zip(y_true, y_pred) if t == p)
+    accuracy = correct / len(y_true) if y_true else 0
+    print(f"Top-1 Accuracy (Classes 0–10): {accuracy:.4f}")
+
+    # Confusion matrix
+    cm = confusion_matrix(y_true, y_pred, labels=list(range(19)))
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=list(range(19)))
+
+    fig, ax = plt.subplots(figsize=(10,8))  # increase or adjust size
+    disp.plot(cmap='Blues', ax=ax)  # format to avoid too many decimals
+
+    # Tweak label rotation and font size
+    # plt.xticks(ha='right', fontsize=10)
+    plt.yticks(fontsize=10)
+    plt.title('Confusion Matrix (Classes 0–18)', fontsize=12)
+
+    # Ensure there's enough space
+    plt.tight_layout()
+    plt.savefig('./confusion_matrix_with_our_model_on_mpii_eval.png', dpi=300)
+    plt.show()
+    # print(labels)
 
 with open(pred_file_path, 'w', newline='') as f:
     writer = csv.writer(f)
