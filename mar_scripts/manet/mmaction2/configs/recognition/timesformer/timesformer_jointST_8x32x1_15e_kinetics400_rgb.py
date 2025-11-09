a1bf7bb37b7b3f76e0a79094f2554fa0161d7b20
Nishit Nilesh Poddar <npoddar@fsophia>
2025-11-09 19:17:14 +0100

    added for timesformer

--- mar_scripts/manet/mmaction2/configs/recognition/timesformer/timesformer_jointST_8x32x1_15e_kinetics400_rgb.py
+++ mar_scripts/manet/mmaction2/configs/recognition/timesformer/timesformer_jointST_8x32x1_15e_kinetics400_rgb.py
@@ -121,4 +121,4 @@ total_epochs = 500
 
 # runtime settings
 checkpoint_config = dict(interval=1)
-work_dir = './work_dirs/timnesformer_for_mpii_frontal_final'
+work_dir = './work_dirs/timnesformer_for_mpii_frontal_final_testing'
