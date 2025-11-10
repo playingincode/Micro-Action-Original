f7eead98ec764e7e57398d23f4a2d44d46e84f59
Nishit Nilesh Poddar <npoddar@fsophia>
2025-11-10 17:49:50 +0100

    experts without manet

--- mar_scripts/manet/mmaction2/train_runner.sh
+++ mar_scripts/manet/mmaction2/train_runner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #OAR -p esterel30
 #OAR -l host=1/gpu=1,walltime=72:00:00
-#OAR --name unique_experts_with_videomae_cross_attn_cross_entropy_final
+#OAR --name unique_experts_with_videomae_cross_attn_cross_entropy_final_without_manet
 #OAR --stdout nef_logs/%jobname%.%jobid%.out
 #OAR --stderr nef_logs/%jobname%.%jobid%.err
 
