f38e2fd92760980013dad94e5b401a11e9e16080
Nishit Nilesh Poddar <npoddar@esterel43-1.sophia.grid5000.fr>
2025-11-11 16:21:21 +0100

    testing updated for social gesture

---
+++ mar_scripts/manet/mmaction2/test_runner.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#OAR -p esterel30
+#OAR -l host=1/gpu=1,walltime=72:00:00
+#OAR --name all_experts_with_Social_Gesture_without_manet_with_pcan_loss_with_lower_lr_without_first_weight_testing
+#OAR --stdout nef_logs/%jobname%.%jobid%.out
+#OAR --stderr nef_logs/%jobname%.%jobid%.err
+
+source ~/.bashrc
+module load conda/2020.48-python3.8 cuda/12.2 gcc/9.2.0
+
+# Activate conda environment
+source $(conda info --base)/etc/profile.d/conda.sh
+source activate openmmlab|| { echo "Conda environment not found"; exit 1; }
+
+# Display python version and path
+python --version
+which python
+
+# Add to PATH
+export PATH="/home/npoddar/:$PATH"
+
+# Run ffmpeg and nvidia-smi to check availability
+ffmpeg
+nvidia-smi || { echo "NVIDIA driver issue"; exit 1; }
+
+export PATH=/pytorch_env/bin:$PATH
+
+export CUBLAS_WORKSPACE_CONFIG=:4096:8
+
+python -u tools/test.py configs/recognition/manet/manet.py  /srv/storage/stars@storage3.sophia.grid5000.fr/npoddar/MANET_original_six_classes/Micro-Action/mar_scripts/manet/mmaction2/work_dirs/all_experts_with_Social_Gesture_without_manet_with_pcan_loss_with_lower_lr_without_first_weight/epoch_2.pth --out online_evaluation/Social_gesture_withu_manet.pickle
