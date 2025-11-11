7b88b4fd0bb105f7de776378183265a815569625
Nishit Nilesh Poddar <npoddar@fsophia>
2025-11-11 16:40:15 +0100

    testing expanded

--- mar_scripts/manet/mmaction2/test_runner.sh
+++ mar_scripts/manet/mmaction2/test_runner.sh
@@ -28,4 +28,4 @@ export PATH=/pytorch_env/bin:$PATH
 
 export CUBLAS_WORKSPACE_CONFIG=:4096:8
 
-python -u tools/test.py configs/recognition/timesformer/timesformer_jointST_8x32x1_15e_kinetics400_rgb.py /srv/storage/stars@storage3.sophia.grid5000.fr/npoddar/MANET_original_six_classes/Micro-Action/mar_scripts/manet/mmaction2/work_dirs/timnesformer_for_mpii_frontal_final_testing/best_top1_acc_epoch_9.pth --out online_evaluation/timesformer_on_mpii_frontal_testing_test.pickle
+python -u tools/test.py configs/recognition/timesformer/timesformer_jointST_8x32x1_15e_kinetics400_rgb.py /srv/storage/stars@storage3.sophia.grid5000.fr/npoddar/MANET_original_six_classes/Micro-Action/mar_scripts/manet/mmaction2/work_dirs/timnesformer_for_mpii_frontal_final_testing/best_top1_acc_epoch_50.pth --out online_evaluation/timesformer_on_mpii_frontal_testing_test.pickle
