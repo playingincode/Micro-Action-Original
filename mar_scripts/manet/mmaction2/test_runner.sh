00de1dee4a1ac507790f468de720528537b1f828
Nishit Nilesh Poddar <npoddar@fsophia>
2025-11-12 17:13:49 +0100

    updated with paths

--- mar_scripts/manet/mmaction2/test_runner.sh
+++ mar_scripts/manet/mmaction2/test_runner.sh
@@ -11,7 +11,7 @@ module load conda/2020.48-python3.8 cuda/12.2 gcc/9.2.0
 
 # Activate conda environment
 source $(conda info --base)/etc/profile.d/conda.sh
-source activate manet_new_six|| { echo "Conda environment not found"; exit 1; }
+source activate openmmlab|| { echo "Conda environment not found"; exit 1; }
 
 # Display python version and path
 python --version
@@ -28,4 +28,4 @@ export PATH=/pytorch_env/bin:$PATH
 
 export CUBLAS_WORKSPACE_CONFIG=:4096:8
 
-python -u tools/train.py configs/recognition/manet/manet.py 
+python -u tools/test.py configs/recognition/manet/manet.py /srv/storage/stars@storage3.sophia.grid5000.fr/npoddar/Micro-Action/mar_scripts/manet/mmaction2/work_dirs/manet/best_top1_acc_epoch_46.pth --out online_evaluation/manet_testing_on_MA52_val.pickle
