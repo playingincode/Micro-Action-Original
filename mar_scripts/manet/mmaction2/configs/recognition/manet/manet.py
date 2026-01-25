8399db8c44b7a002e410ba5a70c3d7b41be1767c
Nishit Nilesh Poddar <npoddar@fsophia>
2026-01-25 17:05:07 +0100

    test updated

--- mar_scripts/manet/mmaction2/configs/recognition/manet/manet.py
+++ mar_scripts/manet/mmaction2/configs/recognition/manet/manet.py
@@ -88,8 +88,8 @@ data = dict(
         pipeline=val_pipeline),
     test=dict(
         type=dataset_type,
-        ann_file=ann_file_val,
-        data_prefix=data_root_val,
+        ann_file=ann_file_test,
+        data_prefix=data_root_test,
         pipeline=test_pipeline))
 evaluation = dict(
     interval=1, metrics=['top_k_accuracy', 'mean_class_accuracy'])
@@ -100,4 +100,4 @@ optimizer = dict(
 )
 # runtime settings
 checkpoint_config = dict(interval=1)
-work_dir = './work_dirs/unique_experts_with_average_pooling_cross_entropy_final'
+work_dir = './work_dirs/unique_experts_with_average_pooling_cross_entropy_final_test'
